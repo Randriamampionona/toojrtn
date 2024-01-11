4fde5fd7259356a61bc2145ce54ed1551692670b
Tooj Rtn <tojorandriaII474@gmail.com>
2024-01-11 10:34:13 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,19 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { get } from '@vercel/edge-config'
 
+export function middleware(req) {
+  const isMaintenance = true 
+  req.nextUrl.pathname = !isMaintenance ? '/' : '/maintenance';
+  return NextResponse.rewrite(req.nextUrl);
+}
+ 
+// Supports both a single value or an array of matches
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-}
-
-export async function middleware(req) {
-  // Check Edge Config to see if the maintenance page should be shown
-  //const isInMaintenanceMode = await get('isInMaintenanceMode')
-
-  // If in maintenance mode, point the url pathname to the maintenance page
-  //if (isInMaintenanceMode) {
-    req.nextUrl.pathname = `/maintenance`
-
-    // Rewrite to the url
-    return NextResponse.rewrite(req.nextUrl)
-  //}
-}
+};
